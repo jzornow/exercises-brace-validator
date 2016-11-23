996763a23d1ce56959cfc2dd88cd53418e9fafa9
Jeremy Zornow <jeremy@zornow.com>
2016-11-23 09:53:30 -0700

    Build out basic spec, pass with parser built for parens alone

---
+++ lib/braces_validator.rb
@@ -0,0 +1,36 @@
+class BracesValidator
+
+  BRACE_PAIRS = {
+    '(' => ')'
+  }
+
+  ALL_BRACES = /[\(\)]/
+
+  def self.valid?(string)
+    braces = string.scan ALL_BRACES
+    close_brace_stack = []
+
+    while braces.any?
+      cursor = braces.pop
+
+      # if it's a close brace
+      if BRACE_PAIRS.values.include? cursor
+        close_brace_stack.push cursor
+
+      # if it's an open brace
+      elsif BRACE_PAIRS.keys.include? cursor
+        last_close_brace = close_brace_stack.pop
+        return false unless matching_pair?(cursor, last_close_brace)
+      end
+    end
+
+    close_brace_stack.empty?
+  end
+
+  private
+
+  def self.matching_pair?(open, close)
+    BRACE_PAIRS[open] == close
+  end
+
+end
---
+++ spec/braces_validator_spec.rb
@@ -0,0 +1,45 @@
+require 'rspec'
+require 'braces_validator'
+
+describe BracesValidator do
+  describe 'valid?' do
+    def expect_validation_to_be(test_string, expected_value)
+      actual_value = BracesValidator.valid?(test_string)
+      expect(actual_value).to be expected_value
+    end
+
+    context 'given a string with no braces' do
+      it 'returns true' do
+        expect_validation_to_be 'This sentence has no parens.', true
+      end
+    end
+
+    context 'given single, unmatched parens' do
+      it 'returns false if open parentheses do not have close parentheses' do
+        expect_validation_to_be 'This sentence has (only an open paren.', false
+      end
+
+      it 'returns false if close parentheses do not have open parentheses' do
+        expect_validation_to_be 'This sentence has only a close paren).', false
+      end
+    end
+
+    context 'given a string with a single pair of parens' do
+      it 'returns true if parentheses are matched' do
+        expect_validation_to_be 'This sentence has (parens).', true
+      end
+
+      it 'returns false if open and close parentheses come in wrong order' do
+        expect_validation_to_be 'This sentence has) bad (paren order.', false
+      end
+
+      it 'returns false if pair is two open parens' do
+        expect_validation_to_be 'This (sentence has (two open parens.', false
+      end
+
+      it 'returns false if pair is two closed parens' do
+        expect_validation_to_be 'This sentence) has two) close parens.', false
+      end
+    end
+  end
+end
